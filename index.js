6851f274bde1123bbe1358d02e3a01146584f61d
Nazmul Huda <mhnazmul48@gmail.com>
2020-09-27 20:03:49 +0600

    Single Data inserted in MongoDB Database

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+const { response } = require('express');
 const express = require('express')
 const app = express()
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://practice-mongo:rWrChfTQDtH7rG8@cluster0.mx72a.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const uri = "mongodb+srv://practice-mongo:rWrChfTQDtH7rG8@cluster0.mx72a.mongodb.net/practice-mongo?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.get('/',(req,res)=>{
-    res.send("Hello Programmer Nazmul Hdua")
+    res.send("Node Js And MongoDB Programme is Running...")
 })
 
 
 
 
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  const collection = client.db("practice-mongo").collection("crud")
+  
+         collection.insertOne({name:"sania Akter", age:20, institute:"iubat"})
+        .then(result => {
+          console.log("Single User Inserted Successful")
+        })
+
 });
-
-
-
 
 app.listen(3000)
 
