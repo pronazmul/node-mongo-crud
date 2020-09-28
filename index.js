27dc70fa179e4d05321482bb5d752ef128a2c2e7
Nazmul Huda <mhnazmul48@gmail.com>
2020-09-28 21:12:13 +0600

    Data successfully Deleted Form MongoDB

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://practice-mongo:rWrChfTQDtH7rG8@cluster0.mx72a.mongodb.net/practice-mongo?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const ObjectId = require('mongodb').ObjectId
 
 
 app.get('/',(req,res)=>{
@@ -19,23 +20,32 @@ app.get('/',(req,res)=>{
   client.connect(err => {
     const collection = client.db("practice-mongo").collection("crud")
 
-  //Read Data From MongoDB......
-  app.get("/read",(req,res)=>{
-      collection.find({})
-      .toArray((err,document)=>{
-        res.send(document)
-      })
-  })
-
-   //Insert Data in MongoDB....    
+    //Insert Data in MongoDB....    
     app.post("/insertData",(req,res)=>{
       collection.insertOne(req.body)
         .then(result => {
-          res.send("Data Successfully Submitted")
+          res.sendFile(__dirname +'/frontend.html')
         })
     })
 
-  });
+    
+  //Read Data From MongoDB......
+    app.get("/read",(req,res)=>{
+        collection.find({})
+        .toArray((err,document)=>{
+          res.send(document)
+        })
+    })
+
+  //Delete Data from MongoDB....
+    app.delete("/delete/:id",(req,res)=>{
+        collection.deleteOne({_id:ObjectId(req.params.id)})
+        .then(result=>{
+              res.sendFile(__dirname +'/frontend.html')
+        })
+    })
+
+});
 
 app.listen(3000)
 
